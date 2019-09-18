d31c5500119c9ab58754990a42025e73e2d08457
Bin Xin <rhyzix@gmail.com>
2019-09-18 15:54:40 +0800

    add clear

--- src/app.js
+++ src/app.js
@@ -21,18 +21,21 @@ function App() {
   const remove = (folder) => {
     setFolders(folders().filter(f => f !== folder))
   }
+  const clear = () => {
+    setFolders([])
+  }
 
   return <div className="app"
     ondragover={evt => evt.preventDefault()}
     ondrop={drop}
   >
+    <label>FPS:</label>
+    <input type="number" value={fps} oninput={({ target }) => setFps(parseInt(target.value))}/>
+    <button type="button" onclick={clear}>清空</button>
+    <a href={downloadString} download="animations.json">保存</a>
     <ul>
       {each(folders, (folder) => <Folder folder={folder} onRemove={() => remove(folder)}></Folder>)}
     </ul>
-
-    <label>FPS:</label>
-    <input type="number" value={fps} oninput={({ target }) => setFps(parseInt(target.value))}/>
-    <a href={downloadString} download="animations.json">保存</a>
   </div>
 }
 
